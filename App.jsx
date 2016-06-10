e6c78b3632a39e6726ee4854aa1b81400da706dd
je-cg <jecg03@hotmail.com>
2016-06-10 00:57:24 -0700

    modified App.jsx

--- App.jsx
+++ App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import Tabs from './components/Tabs';
+import Content from './components/Content';
 
+var tabData = [
+{ name: 'Tab 1', isActive: true, id: 1},
+{ name: 'Tab 2', isActive: false, id: 2 },
+{ name: 'Tab 3', isActive: false, id: 3 }
+];
 
 class App extends React.Component {
-   
-   render() {
+	constructor() {
+		super(...arguments);
+		this.state = {
+			Selected: tabData[0]
+		};
+	} 
+	render() {
 
-      return (
-         <div>
-            Boiler plate app
-         </div>
-      );
-   }
+		return (
+			<Tabs tabs=tabData />
+			<Content active={this.state.Selected} />
+			);
+	}
 }
 
 
